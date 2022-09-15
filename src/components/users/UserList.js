a42fe2922002182d37bc711869d9ad6ef4117043
Andy <alipinski@mail.com>
2022-09-15 19:53:44 +0200

    adding contextAPI

--- src/components/users/UserList.js
+++ src/components/users/UserList.js
@@ -1,27 +1,14 @@
 import React from 'react';
-import { useEffect, useState } from 'react';
+import { useEffect, useContext } from 'react';
 import User from './User';
+import GithubContext from '../../context/GithubContext';
 
 function UserList() {
-  const [users, setUsers] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const { users, loading, fetchUsers } = useContext(GithubContext);
 
   useEffect(() => {
     fetchUsers();
   }, []);
-
-  const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    });
-
-    const data = await response.json();
-
-    setUsers(data);
-    setLoading(false);
-  };
 
   if (!loading) {
     return (
